e118832079b8b48ccf809938405849b3f5469607
LIN <2904518529@qq.com>
2026-02-03 18:25:12 +0800

    add func4

--- detect_V3.py
+++ detect_V3.py
@@ -11,7 +11,10 @@ import sounddevice as sd
 from scipy.io.wavfile import write as wav_write
 
 
+"""
+    recording detected file
 
+"""
 def func1():
     filename = "V3_detecting.wav"
     duration = 5
@@ -23,6 +26,10 @@ def func1():
     print(f"音频录制完成，已保存为：{filename}")
 
 
+"""
+    use generate.py file to generate detected file fingerprint anchor
+
+"""
 def func2(wav_path):
     rate, data , song_name = gv3.func1(wav_path)
     data_magnitude, data_freq_bins_hz, data_time_bins_s = gv3.func2(rate, data)
@@ -31,7 +38,10 @@ def func2(wav_path):
     anchor_pair, filter_data_times, filter_data_hz, filter_data_db = gv3.func5(filter_data_db, filter_data_hz, filter_data_times)
     return anchor_pair
 
+"""
+    score 1
 
+"""
 def func3(anchor_pair,hash_data):
     slave_data = {}
     slave_score = {} 
@@ -66,8 +76,48 @@ def func3(anchor_pair,hash_data):
 
     sorted_scores = sorted(slave_score.items(), key=lambda x: x[1], reverse=True)
     print(sorted_scores)
-    
-    
+
+"""
+    score 2
+
+"""
+def func4(anchor_pair,hash_data):
+    slave_data = {}
+    slave_score = {} 
+    for anchor in anchor_pair:
+        anchor_time, anchor_hz, target_hz, time_diff = anchor
+        hash_str = f"{anchor_hz}|{target_hz}|{time_diff}"
+        hash_value = hashlib.md5(hash_str.encode()).hexdigest()
+        if hash_value in hash_data:
+            for item in hash_data[hash_value]:
+                item_anchor_time , item_songname = item
+
+                if item_songname in slave_data:
+                    slave_data[item_songname].append((hash_value,item_anchor_time,anchor_time))
+                else:
+                    slave_data.update({item_songname:[(hash_value,item_anchor_time,anchor_time)]})
+    print(len(slave_data))
+    if slave_data:    
+        for name in slave_data:
+            offset_dic = {}
+            for i in range(len(slave_data[name])):
+                slave_time = slave_data[name][i][1]
+                host_time = slave_data[name][i][2]
+                # offset_time = abs(host_time-slave_time)
+                # offset_time = round(offset_time,1)
+                offset_time = round(slave_time-host_time,1)
+                if offset_time in offset_dic:
+                    offset_dic[offset_time] += 1
+                else:
+                    offset_dic.update({offset_time:1})
+            max_offset = max(offset_dic.values())
+            slave_score.update({name:max_offset})
+    sorted_scores = sorted(slave_score.items(), key=lambda x: x[1], reverse=True)
+    print(sorted_scores)
+
+
+
+
 
 if __name__ == "__main__":
     db_path = "generate_V3_data.db"
@@ -78,5 +128,5 @@ if __name__ == "__main__":
         print("start")
         func1()
         anchor_pair = func2("V3_detecting.wav")
-        func3(anchor_pair,hash_data)
+        func4(anchor_pair,hash_data)
         print("end")
